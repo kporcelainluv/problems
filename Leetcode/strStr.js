9123b66532a04f0fb23e2d75f5ffa1cd6dcaca06
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-02 12:38:59 +0300

    day 9

---
+++ Leetcode/strStr.js
@@ -0,0 +1,57 @@
+// attempt 1
+var strStr = function(haystack, needle) {
+  // check for invalid input
+  if ((haystack === "" && needle === "") || needle == "") {
+    console.log("here", { haystack, needle });
+    return 0;
+  }
+  let index = 0;
+  let reserveIndex = 1;
+  let needleIndex = 0;
+  let res = [];
+  //   create while loop
+  while (index < haystack.length) {
+    //   loop through elements if occurance is true
+    if (haystack[index] === needle[needleIndex]) {
+      res.push(index);
+      index += 1;
+      needleIndex += 1;
+      if (res.length === needle.length) {
+        break;
+      }
+      //   else go to next index and loop all over again
+    } else {
+      index = reserveIndex;
+      reserveIndex += 1;
+      res = [];
+      needleIndex = 0;
+    }
+  }
+  console.log(res);
+  if (res.length === needle.length && res.length > 0) {
+    return res[0];
+  }
+  return -1;
+};
+// attempt 2
+var strStr = function(haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+
+  for (let i = 0; i < haystack.length; i++) {
+    let j, k;
+    for (j = 0, k = i; j < needle.length && k < haystack.length; j++, k++) {
+      if (haystack[k] !== needle[j]) {
+        break;
+      }
+    }
+    if (j === needle.length) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+module.exports = strStr;
---
+++ Leetcode/strStr.test.js
@@ -0,0 +1,8 @@
+const strStr = require("./strStr");
+test("strStr", () => {
+  expect(strStr("hello", "ll")).toBe(2);
+  expect(strStr("aaaaaaaaaa", "bba")).toBe(-1);
+  expect(strStr("xxx", "xxy")).toBe(-1);
+  expect(strStr("aaa", "aaaa")).toBe(-1);
+  expect(strStr("mississippi", "issip")).toBe(4);
+});
