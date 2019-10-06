9b501d01fab27e14eb2e2de1b2df35753f741e9f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-10-06 15:35:22 +0300

    update day 12

--- Leetcode/addBinary.js
+++ Leetcode/addBinary.js
@@ -1,80 +1,32 @@
-const convertToBinary = num => {
-  if (num === 0) {
-    return "0";
-  }
-  let res = [];
-  let divRes = parseInt(num);
-  while (divRes >= 1) {
-    if (divRes % 2 > 0) {
-      res.push("1");
-    } else {
-      res.push("0");
-    }
-    divRes = Math.floor(divRes / 2);
-  }
-
-  return res.reverse().join("");
-};
-
-const convertToDecimal = num => {
-  let res = 0;
-  num = num.split("").reverse();
-  for (let i = 0; i < num.length; i++) {
-    res += num[i] * 2 ** i;
-  }
-  return res;
-};
-
 const countBinary = (num1, num2) => {
-  let a = Math.max(num1, num2)
-    .toString()
-    .split("")
-    .reverse();
-  let b = Math.min(num1, num2)
-    .toString()
-    .split("")
-    .reverse();
-  const difference = a.length - b.length;
-  for (let i = 0; i < difference; i++) {
-    b.push("0");
-  }
-  let i = 0;
   const res = [];
-  let prevRemembering = 0;
-  while (i <= a.length) {
-    let count = 0;
-    if (prevRemembering > 0) {
-      count = prevRemembering + parseFloat(a[i]) + parseFloat(b[i]);
-      prevRemembering = 0;
-    } else {
-      count = parseFloat(a[i]) + parseFloat(b[i]);
+  for (
+    let i = num1.length - 1, j = num2.length - 1;
+    i > -1 || j > -1;
+    i--, j--
+  ) {
+    res.push(Number(num1[i] || 0) + Number(num2[j] || 0));
+  }
+  let addOne = false;
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] + Number(addOne) == 2) {
+      res[i] = 0;
+      addOne = true;
+    } else if (res[i] + Number(addOne) == 3) {
+      res[i] = 1;
+      addOne = true;
+    } else if (res[i] + Number(addOne) == 0) {
+      addOne = false;
+      res[i] = 0;
+    } else if (res[i] + Number(addOne) == 1) {
+      addOne = false;
+      res[i] = 1;
     }
-    if (count === 2) {
-      res.push("0");
-      prevRemembering = 1;
-      if (i + 1 === a.length) {
-        res.push(prevRemembering.toString());
-        break;
-      }
-    } else if (count === 3) {
-      res.push("1");
-      prevRemembering = 1;
-      if (i + 1 === a.length) {
-        res.push(prevRemembering.toString());
-        break;
-      }
-    } else {
-      res.push(count.toString());
-    }
-    i += 1;
+  }
+  if (addOne) {
+    res.push(1);
   }
   return res.reverse().join("");
 };
 
-const convertationToBinary = (num1, num2) => {
-  if (num1 == 0 && num2 == 0) {
-    return "0";
-  }
-  return countBinary(num1, num2);
-};
-module.exports = convertationToBinary;
+module.exports = countBinary;
--- Leetcode/addBinary.test.js
+++ Leetcode/addBinary.test.js
@@ -1,15 +1,20 @@
-const convertationToBinary = require("./addBinary");
+const countBinary = require("./addBinary");
 
 test("convertationToBinary", () => {
-  expect(convertationToBinary("11", "1")).toBe("100");
-  expect(convertationToBinary("1010", "1011")).toBe("10101");
-  expect(convertationToBinary("0", "0")).toBe("0");
-  //   expect(
-  //     convertationToBinary(
-  //       "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
-  //       "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
-  //     )
-  //   ).toBe(
-  //     "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000"
-  //   );
+  // expect(convertationToBinary("11", "1")).toBe("100");
+  // expect(convertationToBinary("1010", "1011")).toBe("10101");
+  // expect(convertationToBinary("0", "0")).toBe("0");
+  expect(countBinary("0", "0")).toBe("0");
+  expect(countBinary("0", "1")).toBe("1");
+  expect(countBinary("1", "1")).toBe("10");
+  expect(countBinary("111", "1")).toBe("1000");
+  expect(countBinary("101101", "1011100")).toBe("10001001");
+  expect(
+    countBinary(
+      "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+      "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+    )
+  ).toBe(
+    "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000"
+  );
 });
