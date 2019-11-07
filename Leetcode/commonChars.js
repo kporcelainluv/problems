3607190d6abcaef0995a2ae3092ad80c6a962990
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-07 15:26:51 +0300

    day 35/ update memory usage

--- Leetcode/commonChars.js
+++ Leetcode/commonChars.js
@@ -1,12 +1,14 @@
 var twoSum = function(numbers, target) {
-  let res;
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        res = [i + 1, j + 1];
-      }
+  let start = 0;
+  let finish = numbers.length;
+  while (start < finish) {
+    if (numbers[start] + numbers[finish] === target) {
+      return [start + 1, finish + 1];
+    } else if (numbers[start] + numbers[finish] < target) {
+      start += 1;
+    } else {
+      finish -= 1;
     }
   }
-  return res;
 };
 module.exports = twoSum;
