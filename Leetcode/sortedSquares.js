55e90ead48d01dcf0545bba32cb54800414fdfa7
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-08 14:39:05 +0300

    day 36

--- Leetcode/sortedSquares.js
+++ Leetcode/sortedSquares.js
@@ -1,5 +1,6 @@
 const sortedSquares = function(initialArr) {
-  const arr = initialArr.map(elm => elm ** 2);
-  return arr.sort((a, b) => a - b);
+  const arr = initialArr.map(elm => Math.abs(elm));
+  const sortedArr = arr.sort((a, b) => a - b);
+  return sortedArr.map(elm => elm ** 2);
 };
 module.exports = sortedSquares;
