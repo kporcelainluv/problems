01d9e26ca25174efad25200cba61566fdc762573
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-08 14:36:18 +0300

    day 36

---
+++ Leetcode/sortedSquares.test.js
@@ -0,0 +1,5 @@
+const sortedSquares = require("./sortedSquares");
+test("1", () => {
+  expect(sortedSquares([-4, -1, 0, 3, 10])).toStrictEqual([0, 1, 9, 16, 100]);
+  expect(sortedSquares([-7, -3, 2, 3, 11])).toStrictEqual([4, 9, 9, 49, 121]);
+});
