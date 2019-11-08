93f6d35af939d4df2f53974d124aa20a55c33fd5
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-08 14:37:27 +0300

    day 36/attempt 2, sorting func

--- Leetcode/sortedSquares.js
+++ Leetcode/sortedSquares.js
@@ -1,23 +1,5 @@
-const swapArr = arr => {
-  let index = 0;
-  while (true) {
-    if (index === arr.length) {
-      break;
-    }
-    if (arr[index] > arr[index + 1]) {
-      let elm1 = arr[index];
-      arr[index] = arr[index + 1];
-      arr[index + 1] = elm1;
-      index = 0;
-    } else {
-      index += 1;
-    }
-  }
-  return arr;
-};
-
 const sortedSquares = function(initialArr) {
   const arr = initialArr.map(elm => elm ** 2);
-  return swapArr(arr);
+  return arr.sort((a, b) => a - b);
 };
 module.exports = sortedSquares;
