77c2b8a216c571432c2eb6fda96468dbeaca830b
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-10 20:46:50 +0300

    fix max2 assignment

--- coursera_algorithms/maxPairwiseProd.js
+++ coursera_algorithms/maxPairwiseProd.js
@@ -23,8 +23,10 @@ const getMaxNum = arr => {
   let max2 = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max1) {
+      max2 = max1;
       max1 = arr[i];
-    } else if (arr[i] < max1 && arr[i] > max2) {
+    }
+    if (arr[i] < max1 && arr[i] > max2) {
       max2 = arr[i];
     }
   }
