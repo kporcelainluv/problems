4f48776637f61c065a5cbe71722f19a22c416fd3
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-10 20:50:52 +0300

    fix equal values

--- coursera_algorithms/maxPairwiseProd.js
+++ coursera_algorithms/maxPairwiseProd.js
@@ -25,8 +25,7 @@ const getMaxNum = arr => {
     if (arr[i] > max1) {
       max2 = max1;
       max1 = arr[i];
-    }
-    if (arr[i] < max1 && arr[i] > max2) {
+    } else if (arr[i] <= max1 && arr[i] >= max2) {
       max2 = arr[i];
     }
   }
