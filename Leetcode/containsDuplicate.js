ac64ee79f001707d5af6e862f3c0b99aaf9707e0
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-14 23:45:28 +0300

    improve memory usage

--- Leetcode/containsDuplicate.js
+++ Leetcode/containsDuplicate.js
@@ -1,12 +1,23 @@
+// var containsDuplicate = function(nums) {
+//   const obj = {};
+//   for (let i of nums) {
+//     if (!obj[i]) {
+//       obj[i] = 1;
+//     } else {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
 var containsDuplicate = function(nums) {
-  const obj = {};
-  for (let i of nums) {
-    if (!obj[i]) {
-      obj[i] = 1;
-    } else {
+  nums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
       return true;
     }
   }
+
   return false;
 };
-console.log(containsDuplicate([1]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
