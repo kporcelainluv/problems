075b2081d7bee1891fbb0d211ab99c415e4455d4
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-18 00:52:25 +0300

    update Last Digit of the Sum of Fibonacci Numbers task

--- coursera_algorithms/lastdigit2.js
+++ coursera_algorithms/lastdigit2.js
@@ -8,23 +8,43 @@ process.stdin.setEncoding("utf8");
 rl.on("line", readLine);
 
 function readLine(line) {
-  console.log(getSumOfLastFibNum(line.split(" ")[0], line.split(" ")[1]));
+  console.log(
+    getSumOfLastFibNum(
+      parseInt(line.split(" ")[0]),
+      parseInt(line.split(" ")[1])
+    )
+  );
   process.exit();
 }
 
 const getSumOfLastFibNum = (start, finish) => {
+  start = start % 60;
+  finish = finish % 60;
+  [start, finish] = start < finish ? [start, finish] : [finish, start];
   let first = 0;
   let second = 1;
-  let sum;
-  let fibsum = 0;
-  for (let i = 1; i <= finish; i++) {
-    sum = (first + second) % 10;
-    second = first;
-    first = sum;
-    if (i >= start) {
-      fibsum += sum;
-      fibsum = fibsum % 10;
+  let third;
+  let startingValue = 0;
+
+  for (let i = 2; i <= finish + 2; i++) {
+    third = (first + second) % 10;
+    if (i === start + 1) {
+      startingValue = third;
     }
+    first = second;
+    second = third;
   }
-  return fibsum;
+
+  let fibStart = 0;
+  if (start === 0) {
+    fibStart = 0;
+  } else {
+    fibStart = startingValue - 1;
+  }
+  const fibEnd = second - 1;
+
+  const res = fibEnd - fibStart;
+  const finalRes = res >= 0 ? res : 10 + res;
+
+  return finalRes % 10;
 };
