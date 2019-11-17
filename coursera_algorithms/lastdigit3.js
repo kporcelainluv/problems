f92eb6234bcadd938923cb998121edfcd8785835
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-17 22:27:56 +0300

    add Last Digit of the Sum of Squares of Fibonacci Numbers task

---
+++ coursera_algorithms/lastdigit3.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
+
+function readLine(line) {
+  console.log(getSumOfLastFibNum(parseInt(line)));
+  process.exit();
+}
+
+const getSumOfLastFibNum = num => {
+  let first = 1;
+  let second = 1;
+  let third = 1;
+  for (let i = 0; i < num - 1; i++) {
+    third = (first + second) % 10;
+    first = second;
+    second = third;
+  }
+  return (first * second) % 10;
+};
