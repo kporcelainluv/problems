8b425b8185b734cf12b259971ab25eb42144520b
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-17 14:56:03 +0300

    update fibonacci mod algorithm

--- coursera_algorithms/fibonaccimod.js
+++ coursera_algorithms/fibonaccimod.js
@@ -8,50 +8,28 @@ process.stdin.setEncoding("utf8");
 rl.on("line", readLine);
 
 function readLine(line) {
-  // console.log(findPattern(line.split(" ")[0], line.split(" ")[1]));
-  console.log(getFibMod(239, 1000));
+  console.log(getFibMod(line.split(" ")[0], line.split(" ")[1]));
   process.exit();
 }
 
-function fib(num) {
-  if (num < 2) {
-    return num;
-  }
-  let first = 1;
-  let second = 1;
-  let index = 2;
-  let third;
-  while (index < num) {
-    third = second + first;
-    second = first;
-    first = third;
-
-    index += 1;
-  }
-  return first;
-}
-
 const findPattern = (number, mod) => {
-  let res = [0, 1];
-  for (let i = 2; i <= number; i++) {
-    let currentMod = fib(i) % mod;
-    if (currentMod === 0 && fib(i + 1) % mod === 1) {
+  let fibNumbers = [0, 1];
+  let i = 2;
+  while (true) {
+    let newFibNumberMod = (fibNumbers[i - 1] + fibNumbers[i - 2]) % mod;
+    if (newFibNumberMod === 1 && fibNumbers[i - 1] === 0) {
       break;
     }
-    res.push(currentMod);
+    fibNumbers.push(newFibNumberMod);
+    i += 1;
   }
-  return res;
+  fibNumbers.pop();
+  return fibNumbers;
 };
 
 const getFibMod = (number, mod) => {
-  const res = findPattern(number, mod);
-  console.log({ res: new Set(res) });
-  console.log("length", res.length);
-  const numberMod = Math.floor(number / res.length);
-  console.log({ numberMod });
-  const updatedRes = number - numberMod * res.length;
-  console.log({ updatedRes });
-  const answer = fib(updatedRes) % mod;
-  console.log(answer);
+  const pattern = findPattern(number, mod);
+  const index = number % pattern.length;
+  return pattern[index];
 };
 module.exports = getFibMod;
