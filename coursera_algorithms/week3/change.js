8afcaaac70811b09815cf09cb1ba1b17f7d3c9ae
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-18 21:35:51 +0300

    add money change task

---
+++ coursera_algorithms/week3/change.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
+
+function readLine(line) {
+  console.log(getMoneyChange(parseInt(line)));
+  process.exit();
+}
+const getMoneyChange = num => {
+  const moneyChange = [10, 5, 1];
+  let coinsAmount = 0;
+  let index = 0;
+  while (num > 0) {
+    const currentChangeAmount = moneyChange[index];
+
+    if (num >= currentChangeAmount) {
+      num -= currentChangeAmount;
+      coinsAmount += 1;
+    }
+    if (currentChangeAmount > num) {
+      index += 1;
+    }
+  }
+  console.log(coinsAmount);
+};
