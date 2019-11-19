e615811693d004547123b6a5270150c9b0d7bed7
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-19 23:20:36 +0300

    add attempt

---
+++ coursera_algorithms/week3/covering_segments.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
+
+function readLine(line) {
+  console.log(getSumOfLastFibNum(parseInt(line)));
+  process.exit();
+}
+
+const
