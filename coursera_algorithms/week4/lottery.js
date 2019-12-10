36972ed609970b576c3f9e2ae0120ae86fabc6ea
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-11 00:07:20 +0300

    update algorithm

--- coursera_algorithms/week4/lottery.js
+++ coursera_algorithms/week4/lottery.js
@@ -27,19 +27,63 @@ rl.once("line", line => {
 });
 
 const getGapNum = (gaps, nums) => {
-  let ind = 0;
-  let res = 0;
-  let res2 = [];
-  while (ind < nums.length) {
-    gaps.forEach((elm, index) => {
-      if (nums[ind] >= elm[0] && nums[ind] <= elm[1]) {
-        res = index + 1;
-      }
-    });
-
-    res2.push(res);
-    res = 0;
-    ind += 1;
+  let res = [];
+  for (let i of nums) {
+    res.push([i, "p"]);
   }
-  return res2.join(" ");
+  for (let i of gaps) {
+    res.push([i[0], "l"]);
+    res.push([i[1], "r"]);
+  }
+  res = res.sort((a, b) => {
+    if (a[0] > b[0]) {
+      return 1;
+    } else if (a[0] < b[0]) {
+      return -1;
+    } else {
+      if (a[1] === "p") {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  });
+  // console.log({ res });
+  let index = 0;
+  let left = false;
+  let right = false;
+  let onHold = [];
+  let finalRes = [];
+  for (let i = 0; i < res.length; i++) {
+    if (res[i][1] === "p" && left === false) {
+      finalRes = [...finalRes, [res[i][0], 0]];
+    }
+    if (res[i][1] === "l") {
+      left = true;
+      right = false;
+      index += 1;
+    }
+    if (res[i][1] === "p" && left === true) {
+      //num, index
+      onHold = [...onHold, [res[i][0], index]];
+    }
+    if (res[i][1] === "r") {
+      right = true;
+      left = false;
+      finalRes = [...finalRes, ...onHold];
+      onHold = [];
+    }
+    // console.log({ finalRes });
+  }
+
+  let f = finalRes.sort(function(a, b) {
+    return nums.indexOf(a[0]) - nums.indexOf(b[0]);
+  });
+
+  return f.map(elm => elm[1]).join(" ");
+  // console.log({ f: f.map(elm => elm[1]) });
 };
+
+// getGapNum([[-10, 10]], [-100, 100, 0]);
+// getGapNum([[0, 5], [7, 10]], [1, 6, 11]);
+// getGapNum([[0, 5], [-3, 2], [7, 10]], [1, 6]);
